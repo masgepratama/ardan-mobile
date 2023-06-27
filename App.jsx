269bba4137cb8e070748c91c9d862804fc2bc1a9
ardazh <rifaardan@gmail.com>
2023-06-27 17:18:48 +0700

    install package navigation

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {View, Text} from 'react-native';
+import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
